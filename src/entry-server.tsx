31603286d492b29c9b8db3e2a16467041d784ff9
vaibhav <vaibhav@upbytes.in>
2026-03-21 19:37:23 +0530

    add github action

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,12 +2,18 @@ import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom/server";
 import AppShell from "./AppShell";
 import { getPageSeo, prerenderRoutes } from "./seo";
+import { ROUTER_BASENAME } from "./site";
 
 export { prerenderRoutes };
 
 export const render = (url: string) => {
+  const routerUrl =
+    ROUTER_BASENAME === "/"
+      ? url
+      : `${ROUTER_BASENAME}${url === "/" ? "" : url}`;
+
   const appHtml = renderToString(
-    <StaticRouter location={url}>
+    <StaticRouter location={routerUrl} basename={ROUTER_BASENAME}>
       <AppShell />
     </StaticRouter>,
   );
